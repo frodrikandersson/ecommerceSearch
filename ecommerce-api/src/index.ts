038fa0ae228e6993d2e1e9cf0be6aa0308dfcabd
Fredrik Andersson <frodrikandersson@gmail.com>
2025-04-03 01:02:14 +0200

    dwa

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
   // origin: "http://localhost:5173",
-  origin: "*",
+  origin: "https://ecommerce-search-mu.vercel.app/",
   credentials: true,  // ✅ Allows cookies
 }));
 
