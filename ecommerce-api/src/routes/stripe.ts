534a02310fc7ad35593cb5bb7903c77d9def5310
Fredrik Andersson <frodrikandersson@gmail.com>
2025-04-02 21:51:24 +0200

    init

--- ecommerce-api/src/routes/stripe.ts
+++ ecommerce-api/src/routes/stripe.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createStripeHosted, getStripeSession } from "../controllers/stripeController.ts";
+import { createStripeHosted, getStripeSession } from "../controllers/stripeController";
 const router = express.Router();
 
 router.post("/create-checkout-session-hosted", createStripeHosted);
